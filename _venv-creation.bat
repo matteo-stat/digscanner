e62b1011dc7885c098b4e04076f516f669163398
matteo-stat <fogliata.teo@gmail.com>
2022-10-02 14:10:01 +0200

    initial commit

--- _venv-creation.bat
+++ _venv-creation.bat
@@ -1,3 +1,3 @@
 cd /D "%~dp0"
-C:\Users\FOM5CER\AppData\Local\Programs\Python\Python39\python.exe -m venv venv
+C:\Users\Matteo\AppData\Local\Programs\Python\Python39\python.exe -m venv venv
 @pause
